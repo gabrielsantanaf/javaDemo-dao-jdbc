c1049b49e3ffd38a91a668e764f431e8c443e961
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-03-14 11:09:04 -0300

    feat: Implementation of the delete method in object seller.

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -44,5 +47,14 @@ public class Program {
 
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n====TEST 5: Delete seller in database====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted completed");
+
+        sc.close();
+
     }
 }
