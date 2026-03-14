86b46e1ae63f1d91f164e437de3765662f26d802
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-03-14 01:16:44 -0300

    feat: Implementação de método que busca todos os sellers de um department por um id.

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
 
@@ -12,7 +14,14 @@ public class Program {
 
         System.out.println("====TEST 1: Buscando por Id====");
         Seller seller = sellerDao.findById(1);
-
         System.out.println(seller);
+
+        System.out.println("\n====TEST 2: seller findByDepartment====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
